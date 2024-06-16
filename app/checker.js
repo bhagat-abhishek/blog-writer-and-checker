dd91ae4a9ca63f2dd96d143c42a0f4bccf97a426
bhagat-abhishek <inst.abhishek@gmail.com>
2024-06-17 00:38:21 +0530

    reverting back the checker

--- app/checker.js
+++ app/checker.js
@@ -31,19 +31,7 @@ puppeteer.use(
 );
 
 export async function getCheck(text) {
-  const browser = await puppeteer.launch({
-    args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "-single-process",
-      "--no-zygote",
-    ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-    headless: true,
-  });
+  const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
   await page.goto("https://www.duplichecker.com", {
     waitUntil: "networkidle2",
